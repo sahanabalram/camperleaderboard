3dbab26cbe0e57a33c3aef398ef31d5ff5bf4114
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 16:58:00 -0700

    Title Component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Title extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Camper Leaderboard</h1>
+            </div>
+        )
+    }
+}
+export default Title;
