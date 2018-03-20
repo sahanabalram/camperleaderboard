7db1b27de5774d7570fe5725252e0dee6b6c66b7
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 17:03:34 -0700

    Table Component

---
+++ src/components/Table/Table.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Table extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Table Component</h1>
+            </div>
+        )
+    }
+}
+export default Table;
