abea2df602cf2aa8fa0a2a812bc84fb725faf0fd
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 17:20:26 -0700

    Create Axios(Ajax) call

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,6 +1,22 @@
 import React, {Component} from "react";
+import axios from "axios";
 
-class Table extends Component{
+class Table extends Component {
+    state = {
+        top100Days: [],
+        top100AllTime: []
+    }
+    getData(url,stateName) {
+        axios.get(url).then(({data}) => {
+            this.setState({[stateName]:data});
+            console.log(this.state.top100Days);
+            console.log(this.state.top100AllTime);
+        });
+    }
+componentDidMount() {
+    this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/recent","top100Days");
+    this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime","top100AllTime");
+}
     render() {
         return (
             <div>
