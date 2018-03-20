a5fbcdc5ee8ff10f115cc566f17ffceaf1282ea2
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 17:05:33 -0700

    Import Table Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Title from "./components/Title";
-
+import Table from "./components/Table";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Title/>
+        <Table/>
       </div>
     );
   }
