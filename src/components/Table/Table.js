2eeee18074c44d013f241a340b05fc59cea38046
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 17:54:51 -0700

    Create if statement

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,12 +2,14 @@ import React, {Component} from "react";
 import axios from "axios";
 import Table from "react-bootstrap/lib/Table";
 import Image from "react-bootstrap/lib/Image";
+import "font-awesome/css/font-awesome.css";
 import "./Table.css";
 
 class TableContainer extends Component {
     state = {
         top100Days: [],
-        top100AllTime: []
+        top100AllTime: [],
+        current: true
     }
     getData(url, stateName) {
         axios
@@ -18,13 +20,18 @@ class TableContainer extends Component {
                 console.log(this.state.top100AllTime);
             });
     }
+
+    toggleBetweenPoints(value) {
+        if(this.state.current !== value) {
+            this.setState({current: value});
+        }
+    }
     componentDidMount() {
         this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/recent", "top100Days");
         this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime", "top100AllTime");
     }
     render() {
-        const {top100Days} = this.state;
-        const {top100AllTime} = this.state;
+        const {top100Days,top100AllTime,current} = this.state;
         return (
             <div className="container">
                 <Table striped bordered condensed hover className="colorBlack">
@@ -32,12 +39,12 @@ class TableContainer extends Component {
                         <tr>
                             <th>#</th>
                             <th>Camper Name</th>
-                            <th>Points in past 30 Days</th>
-                            <th>All Time Points</th>
+                            <th onClick={(event) => this.toggleBetweenPoints(true)}>Points in past 30 Days {current && <i className="fa fa-caret-down"></i>}</th>
+                            <th onClick={(event) => this.toggleBetweenPoints(false)}>All Time Points {current === false && <i className="fa fa-caret-down"></i>}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {top100Days.map((row, index) => (
+                        {current && (top100Days.map((row, index) => (
                             <tr key={row.username}>
                                 <td>{index + 1}</td>
                                 <td>
@@ -48,6 +55,21 @@ class TableContainer extends Component {
                                 <td>{row.recent}</td>
                                 <td>{row.alltime}</td>
                             </tr>
+                        )
+                        ))}
+
+                        {current===false && (top100AllTime.map((row, index) => (
+                            <tr key={row.username}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <a href={`https:freecodecamp.org/${row.username}`}>
+                                    <Image src={row.img} className="imgHeight" circle/> {row.username}
+                                    </a>
+                                </td>
+                                <td>{row.recent}</td>
+                                <td>{row.alltime}</td>
+                            </tr>
+                        )
                         ))}
                     </tbody>
                 </Table>
