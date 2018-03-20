114f0eb6e9158af53a297463ffc2f02b4209428f
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 17:31:48 -0700

    Create table header

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,29 +1,43 @@
 import React, {Component} from "react";
 import axios from "axios";
-import {Table} from "react-bootstrap";
+import Table from "react-bootstrap/lib/Table";
+import "./Table.css";
 
-class Table extends Component {
+class TableContainer extends Component {
     state = {
         top100Days: [],
         top100AllTime: []
     }
-    getData(url,stateName) {
-        axios.get(url).then(({data}) => {
-            this.setState({[stateName]:data});
-            console.log(this.state.top100Days);
-            console.log(this.state.top100AllTime);
-        });
+    getData(url, stateName) {
+        axios
+            .get(url)
+            .then(({data}) => {
+                this.setState({[stateName]: data});
+                console.log(this.state.top100Days);
+                console.log(this.state.top100AllTime);
+            });
     }
-componentDidMount() {
-    this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/recent","top100Days");
-    this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime","top100AllTime");
-}
+    componentDidMount() {
+        this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/recent", "top100Days");
+        this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime", "top100AllTime");
+    }
     render() {
         return (
             <div>
-                <h1>Table Component</h1>
+                <Table striped bordered condensed hover className="colorBlack">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Camper Name</th>
+                            <th>Points in past 30 Days</th>
+                            <th>All Time Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </Table>
             </div>
         )
     }
 }
-export default Table;
+export default TableContainer;
