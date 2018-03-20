23025c13de2cab17bf7b32d032052ec75dbc226e
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 17:43:48 -0700

    Create top100days table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import axios from "axios";
 import Table from "react-bootstrap/lib/Table";
+import Image from "react-bootstrap/lib/Image";
 import "./Table.css";
 
 class TableContainer extends Component {
@@ -22,8 +23,10 @@ class TableContainer extends Component {
         this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime", "top100AllTime");
     }
     render() {
+        const {top100Days} = this.state;
+        const {top100AllTime} = this.state;
         return (
-            <div>
+            <div className="container">
                 <Table striped bordered condensed hover className="colorBlack">
                     <thead>
                         <tr>
@@ -34,6 +37,18 @@ class TableContainer extends Component {
                         </tr>
                     </thead>
                     <tbody>
+                        {top100Days.map((row, index) => (
+                            <tr key={row.username}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <a href={`https:freecodecamp.org/${row.username}`}>
+                                    <Image src={row.img} className="imgHeight" circle/> {row.username}
+                                    </a>
+                                </td>
+                                <td>{row.recent}</td>
+                                <td>{row.alltime}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </Table>
             </div>
